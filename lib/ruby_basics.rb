6f64a076884f67f4638e721d701ed2499a0b587f
Jennifer Dirmeyer <dirmeyer@gmail.com>
2019-03-22 21:50:10 +0000

    Automatically backed up by Learn

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -2,26 +2,27 @@ def division(num1, num2)
  num1/num2
 end
 
-def assign_variable(value)
-  
+def assign_variable(name)
+  variable = name
 end
 
 def argue
   
 end
 
-def greeting
+def greeting(greeting, name)
 
 end
 
-def return_a_value
-
+def return_a_value(value)
+return value
 end
 
 def last_evaluated_value
+  return expert
 
 end
 
-def pizza_party
-
+def pizza_party(topping = cheese)
+return topping
 end
